de32a79fd2ec78cb26aeec3de61ef128ba23256c
kobv4279 <kobv4278@gmail.com>
2021-08-02 11:56:58 +0900

    git 1

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,12 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+//다른파일 안에서도 쓸수 있게 moviedgAPI 정의함
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+
+export const API_KEY = 'f967cf699952165b7a08a81f69e193a7'
+
+//f967cf699952165b7a08a81f69e193a7
